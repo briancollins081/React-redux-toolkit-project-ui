bf331cca610353ab7c0a2b0e20c4c25f1de4f4e5
Brian Andere <briancollins081@gmail.com>
2020-10-05 22:39:29 +0300

    View post edits

--- src/app/components/cards/postview/PostView.js
+++ src/app/components/cards/postview/PostView.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Card, Image } from 'semantic-ui-react';
 import { API_BASE_URL } from '../../../constants/api';
-import { retrivePostById, retrivePosts, selectPostById, selectPostsCount } from '../../../features/posts/postsSlice';
+import { retrivePosts, selectPostById } from '../../../features/posts/postsSlice';
 import { UncontrollableLoader } from '../../loaders/Loader';
+
+import './PostView.css';
 
 export const PostView = ({ postId }) => {
     const dispatch = useDispatch();
 
     const post = useSelector(state => selectPostById(state, postId));
-    
+
     const loadingStatus = useSelector(state => state.posts.status);
     useEffect(() => {
         if (loadingStatus === 'idle') {
@@ -22,11 +24,11 @@ export const PostView = ({ postId }) => {
     return (
         <>
             {loadingStatus === 'loading' && <UncontrollableLoader />}
-            {loadingStatus === 'success' && <Card fluid>
+            {loadingStatus === 'success' && <Card fluid className="no-card-shadow">
                 <Image src={API_BASE_URL + post?.image} fluid />
-                <Card.Content className="px-40">
+                <Card.Content className="px-40" id="post-sections-container">
                     <Card.Header className="my-20">{post?.title}</Card.Header>
-                    <Card.Description className="mb-40">
+                    <Card.Description className="mb-40 post-section">
                         {post?.body}
                     </Card.Description>
                 </Card.Content>
